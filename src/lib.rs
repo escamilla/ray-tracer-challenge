847b34680a09426ed4ecc0c17a331ccd860f75a4
= <joshua.escamilla@selu.edu>
2019-03-05 23:16:20 -0600

    Implement tuple addition and subtraction

--- src/lib.rs
+++ src/lib.rs
@@ -2,6 +2,6 @@ pub mod tuples;
 
 const EPSILON: f32 = 0.00001;
 
-fn equal(a: f32, b: f32) -> bool {
+fn f32_equal(a: f32, b: f32) -> bool {
     (a - b).abs() < EPSILON
 }
--- src/tuples.rs
+++ src/tuples.rs
@@ -1,3 +1,7 @@
+use super::f32_equal;
+use std::ops::{Add, Sub};
+
+#[derive(Debug)]
 struct Tuple {
     x: f32,
     y: f32,
@@ -27,40 +31,103 @@ impl Tuple {
     }
 }
 
+impl PartialEq for Tuple {
+    fn eq(&self, other: &Tuple) -> bool {
+        f32_equal(self.x, other.x)
+            && f32_equal(self.y, other.y)
+            && f32_equal(self.z, other.z)
+            && f32_equal(self.w, other.w)
+    }
+}
+
+impl Add for Tuple {
+    type Output = Tuple;
+
+    fn add(self, other: Tuple) -> Self {
+        Tuple::new(
+            self.x + other.x,
+            self.y + other.y,
+            self.z + other.z,
+            self.w + other.w,
+        )
+    }
+}
+
+impl Sub for Tuple {
+    type Output = Tuple;
+
+    fn sub(self, other: Tuple) -> Self {
+        Tuple::new(
+            self.x - other.x,
+            self.y - other.y,
+            self.z - other.z,
+            self.w - other.w,
+        )
+    }
+}
+
 #[test]
 fn tuple_with_w_of_1_is_a_point() {
-    let tuple = Tuple::new(4.3, -4.2, 3.1, 1.0);
-    assert_eq!(tuple.x, 4.3);
-    assert_eq!(tuple.y, -4.2);
-    assert_eq!(tuple.z, 3.1);
-    assert_eq!(tuple.w, 1.0);
-    assert!(tuple.is_point());
-    assert!(!tuple.is_vector());
+    let a = Tuple::new(4.3, -4.2, 3.1, 1.0);
+    assert_eq!(a.x, 4.3);
+    assert_eq!(a.y, -4.2);
+    assert_eq!(a.z, 3.1);
+    assert_eq!(a.w, 1.0);
+    assert!(a.is_point());
+    assert!(!a.is_vector());
 }
 
 #[test]
 fn tuple_with_w_of_0_is_a_vector() {
-    let tuple = Tuple::new(4.3, -4.2, 3.1, 0.0);
-    assert_eq!(tuple.x, 4.3);
-    assert_eq!(tuple.y, -4.2);
-    assert_eq!(tuple.z, 3.1);
-    assert_eq!(tuple.w, 0.0);
-    assert!(tuple.is_vector());
-    assert!(!tuple.is_point());
+    let a = Tuple::new(4.3, -4.2, 3.1, 0.0);
+    assert_eq!(a.x, 4.3);
+    assert_eq!(a.y, -4.2);
+    assert_eq!(a.z, 3.1);
+    assert_eq!(a.w, 0.0);
+    assert!(a.is_vector());
+    assert!(!a.is_point());
 }
 
 #[test]
 fn point_function_creates_tuples_with_w_of_1() {
-    let tuple = Tuple::new_point(4.0, -4.0, 3.0);
-    assert_eq!(tuple.w, 1.0);
-    assert!(tuple.is_point());
-    assert!(!tuple.is_vector());
+    let p = Tuple::new_point(4.0, -4.0, 3.0);
+    assert_eq!(p.w, 1.0);
+    assert!(p.is_point());
+    assert!(!p.is_vector());
 }
 
 #[test]
 fn vector_function_creates_tuples_with_w_of_0() {
-    let tuple = Tuple::new_vector(4.0, -4.0, 3.0);
-    assert_eq!(tuple.w, 0.0);
-    assert!(tuple.is_vector());
-    assert!(!tuple.is_point());
+    let v = Tuple::new_vector(4.0, -4.0, 3.0);
+    assert_eq!(v.w, 0.0);
+    assert!(v.is_vector());
+    assert!(!v.is_point());
+}
+
+#[test]
+fn adding_two_tuples() {
+    let a1 = Tuple::new(3.0, -2.0, 5.0, 1.0);
+    let a2 = Tuple::new(-2.0, 3.0, 1.0, 0.0);
+    assert_eq!(a1 + a2, Tuple::new(1.0, 1.0, 6.0, 1.0));
+}
+
+#[test]
+fn subtracting_two_points() {
+    let p1 = Tuple::new_point(3.0, 2.0, 1.0);
+    let p2 = Tuple::new_point(5.0, 6.0, 7.0);
+    assert_eq!(p1 - p2, Tuple::new_vector(-2.0, -4.0, -6.0));
+}
+
+#[test]
+fn subtracting_a_vector_from_a_point() {
+    let p = Tuple::new_point(3., 2., 1.);
+    let v = Tuple::new_vector(5., 6., 7.);
+    assert_eq!(p - v, Tuple::new_point(-2.0, -4.0, -6.0));
+}
+
+#[test]
+fn subtracting_two_vectors() {
+    let v1 = Tuple::new_vector(3.0, 2.0, 1.0);
+    let v2 = Tuple::new_vector(5.0, 6.0, 7.0);
+    assert_eq!(v1 - v2, Tuple::new_vector(-2.0, -4.0, -6.0));
 }
