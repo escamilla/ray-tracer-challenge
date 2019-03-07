47e1a018d0d965b8b23a5d95de225296bad7bc87
= <joshua.escamilla@selu.edu>
2019-03-07 01:16:30 -0600

    Add method to convert canvas to PPM image format

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,4 +1,7 @@
+use crate::i32_clamp;
 use crate::tuples::Tuple;
+
+const PPM_LINE_LENGTH: u32 = 70;
 
 struct Canvas {
     width: u32,
@@ -23,10 +26,46 @@ impl Canvas {
     pub fn pixel_at(&self, x: u32, y: u32) -> Tuple {
         self.pixels[((y * self.width) + x) as usize]
     }
+
+    pub fn to_ppm(&self) -> String {
+        let mut ppm = String::new();
+        ppm.push_str(format!("P3\n{} {}\n255\n", self.width, self.height).as_str());
+        let mut values: Vec<i32> = Vec::new();
+        for (i, pixel) in self.pixels.iter().enumerate() {
+            let scaled_color = *pixel * 255.0;
+            let red = i32_clamp(scaled_color.red().round() as i32, 0, 255);
+            let green = i32_clamp(scaled_color.green().round() as i32, 0, 255);
+            let blue = i32_clamp(scaled_color.blue().round() as i32, 0, 255);
+            values.push(red);
+            values.push(green);
+            values.push(blue);
+        }
+        let mut line = String::new();
+        for i in 0..values.len() {
+            let value = format!("{}", values[i]);
+            if line.len() + 1 + value.len() >= (PPM_LINE_LENGTH as usize) {
+                line.push('\n');
+                ppm.push_str(line.as_str());
+                line = String::new();
+            }
+            if line.len() > 0 {
+                line.push(' ');
+            }
+            line.push_str(value.as_str());
+            if (i + 1) % ((self.width * 3) as usize) == 0 {
+                line.push('\n');
+                ppm.push_str(line.as_str());
+                line = String::new();
+            }
+        }
+        line.push('\n');
+        ppm.push_str(line.as_str());
+        ppm
+    }
 }
 
 #[test]
-pub fn test_creating_a_canvas() {
+fn test_creating_a_canvas() {
     let c = Canvas::new(10, 20);
     assert_eq!(c.width, 10);
     assert_eq!(c.height, 20);
@@ -36,9 +75,70 @@ pub fn test_creating_a_canvas() {
 }
 
 #[test]
-pub fn test_writing_pixels_to_a_canvas() {
+fn test_writing_pixels_to_a_canvas() {
     let mut c = Canvas::new(10, 20);
     let red = Tuple::color(1.0, 0.0, 0.0);
     c.write_pixel(2, 3, red);
     assert_eq!(c.pixel_at(2, 3), red);
+}
+
+#[test]
+fn test_constructing_the_ppm_header() {
+    let c = Canvas::new(5, 3);
+    let ppm = c.to_ppm();
+    let mut lines = ppm.lines();
+    assert_eq!(Some("P3"), lines.next());
+    assert_eq!(Some("5 3"), lines.next());
+    assert_eq!(Some("255"), lines.next());
+}
+
+#[test]
+fn test_constructing_the_ppm_pixel_data() {
+    let mut c = Canvas::new(5, 3);
+    let c1 = Tuple::color(1.5, 0.0, 0.0);
+    let c2 = Tuple::color(0.0, 0.5, 0.0);
+    let c3 = Tuple::color(-0.5, 0.0, 1.0);
+    c.write_pixel(0, 0, c1);
+    c.write_pixel(2, 1, c2);
+    c.write_pixel(4, 2, c3);
+    let ppm = c.to_ppm();
+    let mut lines = ppm.lines().skip(3);
+    assert_eq!(Some("255 0 0 0 0 0 0 0 0 0 0 0 0 0 0"), lines.next());
+    assert_eq!(Some("0 0 0 0 0 0 0 128 0 0 0 0 0 0 0"), lines.next());
+    assert_eq!(Some("0 0 0 0 0 0 0 0 0 0 0 0 0 0 255"), lines.next());
+}
+
+#[test]
+fn test_splitting_long_lines_in_ppm_files() {
+    let mut c = Canvas::new(10, 2);
+    for y in 0..c.height {
+        for x in 0..c.width {
+            c.write_pixel(x, y, Tuple::color(1.0, 0.8, 0.6));
+        }
+    }
+    let ppm = c.to_ppm();
+    let mut lines = ppm.lines().skip(3);
+    assert_eq!(
+        Some("255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204"),
+        lines.next()
+    );
+    assert_eq!(
+        Some("153 255 204 153 255 204 153 255 204 153 255 204 153"),
+        lines.next()
+    );
+    assert_eq!(
+        Some("255 204 153 255 204 153 255 204 153 255 204 153 255 204 153 255 204"),
+        lines.next()
+    );
+    assert_eq!(
+        Some("153 255 204 153 255 204 153 255 204 153 255 204 153"),
+        lines.next()
+    );
+}
+
+#[test]
+fn test_ppm_files_are_terminated_by_a_newline_character() {
+    let mut c = Canvas::new(5, 3);
+    let ppm = c.to_ppm();
+    assert!(ppm.ends_with('\n'));
 }
--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,13 @@ const EPSILON: f32 = 0.00001;
 fn f32_equal(a: f32, b: f32) -> bool {
     (a - b).abs() < EPSILON
 }
+
+fn i32_clamp(num: i32, min: i32, max: i32) -> i32 {
+    if num < min {
+        min
+    } else if num > max {
+        max
+    } else {
+        num
+    }
+}
