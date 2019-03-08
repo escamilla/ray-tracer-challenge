dff170618b2b2727557231f39e1dbc55bae1d570
= <joshua.escamilla@selu.edu>
2019-03-08 02:07:26 -0600

    Use vectors to initialize matrices instead of setting individual values

--- src/matrix.rs
+++ src/matrix.rs
@@ -18,6 +18,11 @@ impl Matrix {
         }
     }
 
+    pub fn new_with_values(rows: usize, cols: usize, values: Vec<f32>) -> Self {
+        assert_eq!(rows * cols, values.len());
+        Matrix { rows, cols, values }
+    }
+
     pub fn set_value(&mut self, row: usize, col: usize, value: f32) {
         self.values[row * self.cols + col] = value;
     }
@@ -87,23 +92,16 @@ impl Mul<Tuple> for Matrix {
 
 #[test]
 fn test_constructing_and_inspecting_a_4_x_4_matrix() {
-    let mut m = Matrix::new(4, 4);
-    m.set_value(0, 0, 1.0);
-    m.set_value(0, 1, 2.0);
-    m.set_value(0, 2, 3.0);
-    m.set_value(0, 3, 4.0);
-    m.set_value(1, 0, 5.5);
-    m.set_value(1, 1, 6.5);
-    m.set_value(1, 2, 7.5);
-    m.set_value(1, 3, 8.5);
-    m.set_value(2, 0, 9.0);
-    m.set_value(2, 1, 10.0);
-    m.set_value(2, 2, 11.0);
-    m.set_value(2, 3, 12.0);
-    m.set_value(3, 0, 13.5);
-    m.set_value(3, 1, 14.5);
-    m.set_value(3, 2, 15.5);
-    m.set_value(3, 3, 16.5);
+    #[rustfmt::skip]
+    let m: Matrix = Matrix::new_with_values(
+        4, 4,
+        vec![
+            1.0, 2.0, 3.0, 4.0,
+            5.5, 6.5, 7.5, 8.5,
+            9.0, 10.0, 11.0, 12.0,
+            13.5, 14.5, 15.5, 16.5,
+        ],
+    );
     assert_eq!(m.value_at(0, 0), 1.0);
     assert_eq!(m.value_at(0, 3), 4.0);
     assert_eq!(m.value_at(1, 0), 5.5);
@@ -115,11 +113,7 @@ fn test_constructing_and_inspecting_a_4_x_4_matrix() {
 
 #[test]
 fn a_2_x_2_matrix_ought_to_be_representable() {
-    let mut m = Matrix::new(2, 2);
-    m.set_value(0, 0, -3.0);
-    m.set_value(0, 1, 5.0);
-    m.set_value(1, 0, 1.0);
-    m.set_value(1, 1, -2.0);
+    let m = Matrix::new_with_values(2, 2, vec![-3.0, 5.0, 1.0, -2.0]);
     assert_eq!(m.value_at(0, 0), -3.0);
     assert_eq!(m.value_at(0, 1), 5.0);
     assert_eq!(m.value_at(1, 0), 1.0);
@@ -128,16 +122,15 @@ fn a_2_x_2_matrix_ought_to_be_representable() {
 
 #[test]
 fn a_3_x_3_matrix_ought_to_be_representable() {
-    let mut m = Matrix::new(3, 3);
-    m.set_value(0, 0, -3.0);
-    m.set_value(0, 1, 5.0);
-    m.set_value(0, 2, 0.0);
-    m.set_value(1, 0, 1.0);
-    m.set_value(1, 1, -2.0);
-    m.set_value(1, 2, -7.0);
-    m.set_value(2, 0, 0.0);
-    m.set_value(2, 1, 1.0);
-    m.set_value(2, 2, 1.0);
+    #[rustfmt::skip]
+    let m = Matrix::new_with_values(
+        3, 3,
+        vec![
+            -3.0, 5.0, 0.0,
+            1.0, -2.0, -7.0,
+            0.0, 1.0, 1.0,
+        ],
+    );
     assert_eq!(m.value_at(0, 0), -3.0);
     assert_eq!(m.value_at(0, 1), 5.0);
     assert_eq!(m.value_at(0, 2), 0.0);
@@ -151,180 +144,116 @@ fn a_3_x_3_matrix_ought_to_be_representable() {
 
 #[test]
 fn test_matrix_equality_with_identical_matrices() {
-    let mut a = Matrix::new(4, 4);
-    a.set_value(0, 0, 1.0);
-    a.set_value(0, 1, 2.0);
-    a.set_value(0, 2, 3.0);
-    a.set_value(0, 3, 4.0);
-    a.set_value(1, 0, 5.0);
-    a.set_value(1, 1, 6.0);
-    a.set_value(1, 2, 7.0);
-    a.set_value(1, 3, 8.0);
-    a.set_value(2, 0, 9.0);
-    a.set_value(2, 1, 8.0);
-    a.set_value(2, 2, 7.0);
-    a.set_value(2, 3, 6.0);
-    a.set_value(3, 0, 5.0);
-    a.set_value(3, 1, 4.0);
-    a.set_value(3, 2, 3.0);
-    a.set_value(3, 3, 2.0);
-    let mut b = Matrix::new(4, 4);
-    b.set_value(0, 0, 1.0);
-    b.set_value(0, 1, 2.0);
-    b.set_value(0, 2, 3.0);
-    b.set_value(0, 3, 4.0);
-    b.set_value(1, 0, 5.0);
-    b.set_value(1, 1, 6.0);
-    b.set_value(1, 2, 7.0);
-    b.set_value(1, 3, 8.0);
-    b.set_value(2, 0, 9.0);
-    b.set_value(2, 1, 8.0);
-    b.set_value(2, 2, 7.0);
-    b.set_value(2, 3, 6.0);
-    b.set_value(3, 0, 5.0);
-    b.set_value(3, 1, 4.0);
-    b.set_value(3, 2, 3.0);
-    b.set_value(3, 3, 2.0);
+    #[rustfmt::skip]
+    let a = Matrix::new_with_values(
+        4, 4,
+        vec![
+            1.0, 2.0, 3.0, 4.0,
+            5.0, 6.0, 7.0, 8.0,
+            9.0, 8.0, 7.0, 6.0,
+            5.0, 4.0, 3.0, 2.0,
+        ],
+    );
+    #[rustfmt::skip]
+    let b = Matrix::new_with_values(
+        4, 4,
+        vec![
+            1.0, 2.0, 3.0, 4.0,
+            5.0, 6.0, 7.0, 8.0,
+            9.0, 8.0, 7.0, 6.0,
+            5.0, 4.0, 3.0, 2.0,
+        ],
+    );
     assert_eq!(a, b);
 }
 
 #[test]
 fn test_matrix_equality_with_different_matrices() {
-    let mut a = Matrix::new(4, 4);
-    a.set_value(0, 0, 1.0);
-    a.set_value(0, 1, 2.0);
-    a.set_value(0, 2, 3.0);
-    a.set_value(0, 3, 4.0);
-    a.set_value(1, 0, 5.0);
-    a.set_value(1, 1, 6.0);
-    a.set_value(1, 2, 7.0);
-    a.set_value(1, 3, 8.0);
-    a.set_value(2, 0, 9.0);
-    a.set_value(2, 1, 8.0);
-    a.set_value(2, 2, 7.0);
-    a.set_value(2, 3, 6.0);
-    a.set_value(3, 0, 5.0);
-    a.set_value(3, 1, 4.0);
-    a.set_value(3, 2, 3.0);
-    a.set_value(3, 3, 2.0);
-    let mut b = Matrix::new(4, 4);
-    b.set_value(0, 0, 2.0);
-    b.set_value(0, 1, 3.0);
-    b.set_value(0, 2, 4.0);
-    b.set_value(0, 3, 5.0);
-    b.set_value(1, 0, 6.0);
-    b.set_value(1, 1, 7.0);
-    b.set_value(1, 2, 8.0);
-    b.set_value(1, 3, 9.0);
-    b.set_value(2, 0, 8.0);
-    b.set_value(2, 1, 7.0);
-    b.set_value(2, 2, 6.0);
-    b.set_value(2, 3, 5.0);
-    b.set_value(3, 0, 4.0);
-    b.set_value(3, 1, 3.0);
-    b.set_value(3, 2, 2.0);
-    b.set_value(3, 3, 1.0);
+    let a = Matrix::new_with_values(
+        4, 4,
+        vec![
+            1.0, 2.0, 3.0, 4.0,
+            5.0, 6.0, 7.0, 8.0,
+            9.0, 8.0, 7.0, 6.0,
+            5.0, 4.0, 3.0, 2.0,
+        ],
+    );
+    #[rustfmt::skip]
+    let b = Matrix::new_with_values(
+        4, 4,
+        vec![
+            2.0, 3.0, 4.0, 5.0,
+            6.0, 7.0, 8.0, 9.0,
+            8.0, 7.0, 6.0, 5.0,
+            4.0, 3.0, 2.0, 1.0,
+        ],
+    );
     assert_ne!(a, b);
 }
 
 #[test]
 fn test_multiplying_two_matrices() {
-    let mut a = Matrix::new(4, 4);
-    a.set_value(0, 0, 1.0);
-    a.set_value(0, 1, 2.0);
-    a.set_value(0, 2, 3.0);
-    a.set_value(0, 3, 4.0);
-    a.set_value(1, 0, 5.0);
-    a.set_value(1, 1, 6.0);
-    a.set_value(1, 2, 7.0);
-    a.set_value(1, 3, 8.0);
-    a.set_value(2, 0, 9.0);
-    a.set_value(2, 1, 8.0);
-    a.set_value(2, 2, 7.0);
-    a.set_value(2, 3, 6.0);
-    a.set_value(3, 0, 5.0);
-    a.set_value(3, 1, 4.0);
-    a.set_value(3, 2, 3.0);
-    a.set_value(3, 3, 2.0);
-    let mut b = Matrix::new(4, 4);
-    b.set_value(0, 0, -2.0);
-    b.set_value(0, 1, 1.0);
-    b.set_value(0, 2, 2.0);
-    b.set_value(0, 3, 3.0);
-    b.set_value(1, 0, 3.0);
-    b.set_value(1, 1, 2.0);
-    b.set_value(1, 2, 1.0);
-    b.set_value(1, 3, -1.0);
-    b.set_value(2, 0, 4.0);
-    b.set_value(2, 1, 3.0);
-    b.set_value(2, 2, 6.0);
-    b.set_value(2, 3, 5.0);
-    b.set_value(3, 0, 1.0);
-    b.set_value(3, 1, 2.0);
-    b.set_value(3, 2, 7.0);
-    b.set_value(3, 3, 8.0);
-    let mut m = Matrix::new(4, 4);
-    m.set_value(0, 0, 20.0);
-    m.set_value(0, 1, 22.0);
-    m.set_value(0, 2, 50.0);
-    m.set_value(0, 3, 48.0);
-    m.set_value(1, 0, 44.0);
-    m.set_value(1, 1, 54.0);
-    m.set_value(1, 2, 114.0);
-    m.set_value(1, 3, 108.0);
-    m.set_value(2, 0, 40.0);
-    m.set_value(2, 1, 58.0);
-    m.set_value(2, 2, 110.0);
-    m.set_value(2, 3, 102.0);
-    m.set_value(3, 0, 16.0);
-    m.set_value(3, 1, 26.0);
-    m.set_value(3, 2, 46.0);
-    m.set_value(3, 3, 42.0);
+    #[rustfmt::skip]
+    let a = Matrix::new_with_values(
+        4, 4,
+        vec![
+            1.0, 2.0, 3.0, 4.0,
+            5.0, 6.0, 7.0, 8.0,
+            9.0, 8.0, 7.0, 6.0,
+            5.0, 4.0, 3.0, 2.0,
+        ],
+    );
+    #[rustfmt::skip]
+    let b = Matrix::new_with_values(
+        4, 4,
+        vec![
+            -2.0, 1.0, 2.0, 3.0,
+            3.0, 2.0, 1.0, -1.0,
+            4.0, 3.0, 6.0, 5.0,
+            1.0, 2.0, 7.0, 8.0,
+        ],
+    );
+    #[rustfmt::skip]
+    let m = Matrix::new_with_values(
+        4, 4,
+        vec![
+            20.0, 22.0, 50.0, 48.0,
+            44.0, 54.0, 114.0, 108.0,
+            40.0, 58.0, 110.0, 102.0,
+            16.0, 26.0, 46.0, 42.0,
+        ],
+    );
     assert_eq!(a * b, m);
 }
 
 #[test]
 fn test_a_matrix_multiplied_by_a_tuple() {
-    let mut a = Matrix::new(4, 4);
-    a.set_value(0, 0, 1.0);
-    a.set_value(0, 1, 2.0);
-    a.set_value(0, 2, 3.0);
-    a.set_value(0, 3, 4.0);
-    a.set_value(1, 0, 2.0);
-    a.set_value(1, 1, 4.0);
-    a.set_value(1, 2, 4.0);
-    a.set_value(1, 3, 2.0);
-    a.set_value(2, 0, 8.0);
-    a.set_value(2, 1, 6.0);
-    a.set_value(2, 2, 4.0);
-    a.set_value(2, 3, 1.0);
-    a.set_value(3, 0, 0.0);
-    a.set_value(3, 1, 0.0);
-    a.set_value(3, 2, 0.0);
-    a.set_value(3, 3, 1.0);
+    #[rustfmt::skip]
+    let a = Matrix::new_with_values(
+        4, 4,
+        vec![
+            1.0, 2.0, 3.0, 4.0,
+            2.0, 4.0, 4.0, 2.0,
+            8.0, 6.0, 4.0, 1.0,
+            0.0, 0.0, 0.0, 1.0,
+        ],
+    );
     let b = Tuple::new(1.0, 2.0, 3.0, 1.0);
     assert_eq!(a * b, Tuple::new(18.0, 24.0, 33.0, 1.0));
 }
 
 #[test]
 fn test_multiplying_a_matrix_by_the_identity_matrix() {
-    let mut a = Matrix::new(4, 4);
-    a.set_value(0, 0, 0.0);
-    a.set_value(0, 1, 1.0);
-    a.set_value(0, 2, 2.0);
-    a.set_value(0, 3, 4.0);
-    a.set_value(1, 0, 1.0);
-    a.set_value(1, 1, 2.0);
-    a.set_value(1, 2, 4.0);
-    a.set_value(1, 3, 8.0);
-    a.set_value(2, 0, 2.0);
-    a.set_value(2, 1, 4.0);
-    a.set_value(2, 2, 8.0);
-    a.set_value(2, 3, 16.0);
-    a.set_value(3, 0, 4.0);
-    a.set_value(3, 1, 8.0);
-    a.set_value(3, 2, 16.0);
-    a.set_value(3, 3, 32.0);
+    #[rustfmt::skip]
+    let a = Matrix::new_with_values(
+        4, 4,
+        vec![
+            0.0, 1.0, 2.0, 4.0,
+            1.0, 2.0, 4.0, 8.0,
+            2.0, 4.0, 8.0, 16.0,
+            4.0, 8.0, 16.0, 32.0,
+        ],
+    );
     assert_eq!(a.clone() * Matrix::identity_matrix(4), a);
 }
 
@@ -336,40 +265,26 @@ fn test_multiplying_the_identity_matrix_by_a_tuple() {
 
 #[test]
 fn test_transposing_a_matrix() {
-    let mut a = Matrix::new(4, 4);
-    a.set_value(0, 0, 0.0);
-    a.set_value(0, 1, 9.0);
-    a.set_value(0, 2, 3.0);
-    a.set_value(0, 3, 0.0);
-    a.set_value(1, 0, 9.0);
-    a.set_value(1, 1, 8.0);
-    a.set_value(1, 2, 0.0);
-    a.set_value(1, 3, 8.0);
-    a.set_value(2, 0, 1.0);
-    a.set_value(2, 1, 8.0);
-    a.set_value(2, 2, 5.0);
-    a.set_value(2, 3, 3.0);
-    a.set_value(3, 0, 0.0);
-    a.set_value(3, 1, 0.0);
-    a.set_value(3, 2, 5.0);
-    a.set_value(3, 3, 8.0);
-    let mut b = Matrix::new(4, 4);
-    b.set_value(0, 0, 0.0);
-    b.set_value(0, 1, 9.0);
-    b.set_value(0, 2, 1.0);
-    b.set_value(0, 3, 0.0);
-    b.set_value(1, 0, 9.0);
-    b.set_value(1, 1, 8.0);
-    b.set_value(1, 2, 8.0);
-    b.set_value(1, 3, 0.0);
-    b.set_value(2, 0, 3.0);
-    b.set_value(2, 1, 0.0);
-    b.set_value(2, 2, 5.0);
-    b.set_value(2, 3, 5.0);
-    b.set_value(3, 0, 0.0);
-    b.set_value(3, 1, 8.0);
-    b.set_value(3, 2, 3.0);
-    b.set_value(3, 3, 8.0);
+    #[rustfmt::skip]
+    let a = Matrix::new_with_values(
+        4, 4,
+        vec![
+            0.0, 9.0, 3.0, 0.0,
+            9.0, 8.0, 0.0, 8.0,
+            1.0, 8.0, 5.0, 3.0,
+            0.0, 0.0, 5.0, 8.0,
+        ],
+    );
+    #[rustfmt::skip]
+    let b = Matrix::new_with_values(
+        4, 4,
+        vec![
+            0.0, 9.0, 1.0, 0.0,
+            9.0, 8.0, 8.0, 0.0,
+            3.0, 0.0, 5.0, 5.0,
+            0.0, 8.0, 3.0, 8.0,
+        ],
+    );
     assert_eq!(a.transpose(), b);
 }
 
