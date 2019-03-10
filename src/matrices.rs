42040f3384c6ec8dc3cb7243dab260b6ba77c582
= <joshua.escamilla@selu.edu>
2019-03-10 18:25:53 -0500

    Add tests for order of matrix transformations

--- src/matrices.rs
+++ src/matrices.rs
@@ -813,3 +813,27 @@ fn test_a_shearing_transformation_moves_z_in_proportion_to_y() {
     let p = Tuple::point(2.0, 3.0, 4.0);
     assert_eq!(transform * p, Tuple::point(2.0, 3.0, 7.0));
 }
+
+#[test]
+fn test_individual_transformations_are_applied_in_sequence() {
+    let p = Tuple::point(1.0, 0.0, 1.0);
+    let a = Matrix4::rotation_x(FRAC_PI_2);
+    let b = Matrix4::scaling(5.0, 5.0, 5.0);
+    let c = Matrix4::translation(10.0, 5.0, 7.0);
+    let p2 = a * p;
+    assert_eq!(p2, Tuple::point(1.0, -1.0, 0.0));
+    let p3 = b * p2;
+    assert_eq!(p3, Tuple::point(5.0, -5.0, 0.0));
+    let p4 = c * p3;
+    assert_eq!(p4, Tuple::point(15.0, 0.0, 7.0));
+}
+
+#[test]
+fn test_chained_transformations_must_be_applied_in_reverse_order() {
+    let p = Tuple::point(1.0, 0.0, 1.0);
+    let a = Matrix4::rotation_x(FRAC_PI_2);
+    let b = Matrix4::scaling(5.0, 5.0, 5.0);
+    let c = Matrix4::translation(10.0, 5.0, 7.0);
+    let t = c * b * a;
+    assert_eq!(t * p, Tuple::point(15.0, 0.0, 7.0));
+}
