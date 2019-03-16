1a15112746ac5b38d0e16d568fe32ba39810fcfa
= <joshua.escamilla@selu.edu>
2019-03-15 23:11:39 -0500

    Add a method to return a transformation matrix that orients the world relative to a point

--- src/matrix.rs
+++ src/matrix.rs
@@ -283,6 +283,24 @@ impl Matrix4 {
             [0.0, 0.0, 0.0, 1.0],
         ])
     }
+
+    /// Returns a transformation matrix that orients the world relative
+    /// to the camera. Specify the location of the camera (the `from`
+    /// parameter), the point in the scene at which the camera should
+    /// look (the `to` parameter), and a vector indicating which
+    /// direction is up.
+    pub fn view_transform(from: Tuple, to: Tuple, up: Tuple) -> Matrix4 {
+        let forward = (to - from).normalize();
+        let left = forward.cross(up.normalize());
+        let true_up = left.cross(forward);
+        let orientation = Matrix4::from_rows([
+            [left.x, left.y, left.z, 0.0],
+            [true_up.x, true_up.y, true_up.z, 0.0],
+            [-forward.x, -forward.y, -forward.z, 0.0],
+            [0.0, 0.0, 0.0, 1.0],
+        ]);
+        orientation * Matrix4::translation(-from.x, -from.y, -from.z)
+    }
 }
 
 impl PartialEq for Matrix4 {
@@ -856,4 +874,48 @@ fn test_chained_transformations_must_be_applied_in_reverse_order() {
     let c = Matrix4::translation(10.0, 5.0, 7.0);
     let t = c * b * a;
     assert_eq!(t * p, Tuple::point(15.0, 0.0, 7.0));
+}
+
+#[test]
+fn test_the_transformation_matrix_for_the_default_orientation() {
+    let from = Tuple::point(0.0, 0.0, 0.0);
+    let to = Tuple::point(0.0, 0.0, -1.0);
+    let up = Tuple::vector(0.0, 1.0, 0.0);
+    let t = Matrix4::view_transform(from, to, up);
+    assert_eq!(t, Matrix4::identity());
+}
+
+#[test]
+fn test_a_view_transformation_matrix_looking_in_positive_z_direction() {
+    let from = Tuple::point(0.0, 0.0, 0.0);
+    let to = Tuple::point(0.0, 0.0, 1.0);
+    let up = Tuple::vector(0.0, 1.0, 0.0);
+    let t = Matrix4::view_transform(from, to, up);
+    assert_eq!(t, Matrix4::scaling(-1.0, 1.0, -1.0));
+}
+
+#[test]
+fn test_the_view_transformation_moves_the_world() {
+    let from = Tuple::point(0.0, 0.0, 8.0);
+    let to = Tuple::point(0.0, 0.0, 0.0);
+    let up = Tuple::vector(0.0, 1.0, 0.0);
+    let t = Matrix4::view_transform(from, to, up);
+    assert_eq!(t, Matrix4::translation(0.0, 0.0, -8.0));
+}
+
+#[test]
+fn test_an_arbitrary_view_transformation() {
+    let from = Tuple::point(1.0, 3.0, 2.0);
+    let to = Tuple::point(4.0, -2.0, 8.0);
+    let up = Tuple::vector(1.0, 1.0, 0.0);
+    let t = Matrix4::view_transform(from, to, up);
+    assert_eq!(
+        t,
+        Matrix4::from_rows([
+            [-0.50709, 0.50709, 0.67612, -2.36643],
+            [0.76772, 0.60609, 0.12122, -2.82843],
+            [-0.35857, 0.59761, -0.71714, 0.00000],
+            [0.00000, 0.00000, 0.00000, 1.00000],
+        ])
+    );
 }
